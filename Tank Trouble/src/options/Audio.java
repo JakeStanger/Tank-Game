b85c14459eab6d02ad1bea955b89a9aca7d6e170
Roboguy99 <roboguy99@gmail.com>
2014-01-06 18:58:59 +0000

    Massive tidying of code

--- Tank Trouble/src/options/Audio.java
+++ Tank Trouble/src/options/Audio.java
@@ -11,27 +11,32 @@ import swing.Slider;
 
 public class Audio 
 {
+	JPanel pnlAudio = Window.pnlAudio;
+	Container contentPane = Window.frame.getContentPane();
+	Slider sldMasterSound = Window.sldMasterSound;
+	Label lblMasterSound = Window.lblMasterSound;
+	SpringLayout layout = new SpringLayout();
+	
 	public Audio()
 	{
-		JPanel pnlAudio = Window.pnlAudio;
-		Container contentPane = Window.frame.getContentPane();
-		Slider sldMasterSound = Window.sldMasterSound;
-		Label lblMasterSound = Window.lblMasterSound;
-		
-		SpringLayout layout = new SpringLayout();
 		pnlAudio.setLayout(layout);
 		
+		pnlAudio.add(lblMasterSound);
+		pnlAudio.add(sldMasterSound);
+		
+		positionElements();
+		
+		Window.switchTo("Audio");
+		
+		System.out.println("Audio class loaded");
+	}
+	
+	public void positionElements()
+	{
 		layout.putConstraint(SpringLayout.WEST, lblMasterSound,5, SpringLayout.WEST, contentPane);
 		layout.putConstraint(SpringLayout.NORTH, lblMasterSound,5, SpringLayout.NORTH, contentPane);
 		
 		layout.putConstraint(SpringLayout.WEST, sldMasterSound, 40, SpringLayout.EAST, lblMasterSound);
 		layout.putConstraint(SpringLayout.NORTH, sldMasterSound, 15, SpringLayout.NORTH, contentPane);
-		
-		pnlAudio.add(lblMasterSound);
-		pnlAudio.add(sldMasterSound);
-		
-		Window.switchTo("Audio");
-		
-		System.out.println("Audio class loaded");
 	}
 }
