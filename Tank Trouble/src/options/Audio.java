575f1c2c11d7673ccb8cc7709863ccdcaa42def6
Roboguy84 <Roboguy84@William-Deskotp>
2014-01-07 20:07:34 +0000

    Test message

--- Tank Trouble/src/options/Audio.java
+++ Tank Trouble/src/options/Audio.java
@@ -28,7 +28,7 @@ public class Audio
 	{
 		pnlAudio.setLayout(layout);
 		
-		pnlAudio.add(lblMasterSound);
+		pnlAudio.add(lblMasterSound); //Helodhghrghj
 		pnlAudio.add(sldMasterSound);
 		pnlAudio.add(spnMasterSound);
 		
